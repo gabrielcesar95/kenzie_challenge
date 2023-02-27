45571d6702f004fdd3408151244d8a49e7de29a0
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2023-02-27 15:44:45 -0300

    feat: add typeORM dependency

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './task/entities/task.entity';
 import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TaskModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'todo',
+      entities: [Task],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
