0cdf10749297b5c4b15403ed466743d6523e6280
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2023-03-01 16:45:12 -0300

    test: service and controller tests

--- api/src/task/task.controller.spec.ts
+++ api/src/task/task.controller.spec.ts
@@ -1,4 +1,7 @@
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 
@@ -8,13 +11,172 @@ describe('TaskController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TaskController],
-      providers: [TaskService],
+      providers: [
+        {
+          provide: TaskService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([
+              { title: 'Test Task 1', isChecked: false },
+              { title: 'Test Task 2', isChecked: true },
+              { title: 'Test Task 3', isChecked: false },
+            ]),
+            findOne: jest
+              .fn()
+              .mockImplementation((id: number) =>
+                Promise.resolve({ id, title: 'Test Task 1', isChecked: false }),
+              ),
+            create: jest
+              .fn()
+              .mockImplementation((task: CreateTaskDto) =>
+                Promise.resolve({ id: 10, ...task }),
+              ),
+            update: jest
+              .fn()
+              .mockImplementation((id: number, task: UpdateTaskDto) =>
+                Promise.resolve({ id, ...task }),
+              ),
+            remove: jest.fn().mockImplementation((id: number) =>
+              Promise.resolve({
+                id,
+                title: 'Deleted Task',
+                isChecked: false,
+              }),
+            ),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<TaskController>(TaskController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('definitions', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+
+    it('should have all methods defined', () => {
+      expect(controller.create).toBeDefined();
+      expect(controller.findAll).toBeDefined();
+      expect(controller.findOne).toBeDefined();
+      expect(controller.update).toBeDefined();
+      expect(controller.remove).toBeDefined();
+    });
+  });
+
+  describe('CRUD', () => {
+    describe('Create', () => {
+      it('should create a task', async () => {
+        const newTaskDTO: CreateTaskDto = {
+          title: 'My new task',
+          isChecked: false,
+        };
+        await expect(controller.create(newTaskDTO)).resolves.toEqual({
+          id: 10,
+          ...newTaskDTO,
+        });
+      });
+
+      it('should return error on invalid task creation', async () => {
+        jest
+          .spyOn(controller, 'create')
+          .mockRejectedValueOnce(new BadRequestException());
+
+        const request = {
+          title: null,
+          isChecked: false,
+        };
+
+        await expect(controller.create(request)).rejects.toThrow(
+          BadRequestException,
+        );
+      });
+    });
+
+    describe('Read', () => {
+      describe('getTasks', () => {
+        it('should get all tasks', async () => {
+          await expect(controller.findAll()).resolves.toEqual([
+            { title: 'Test Task 1', isChecked: false },
+            { title: 'Test Task 2', isChecked: true },
+            { title: 'Test Task 3', isChecked: false },
+          ]);
+        });
+      });
+
+      it('should get single task', async () => {
+        await expect(controller.findOne(11)).resolves.toEqual({
+          id: 11,
+          title: 'Test Task 1',
+          isChecked: false,
+        });
+        await expect(controller.findOne(110)).resolves.toEqual({
+          id: 110,
+          title: 'Test Task 1',
+          isChecked: false,
+        });
+      });
+    });
+
+    describe('Update', () => {
+      it('should update a task', async () => {
+        const updatedTaskDTO: UpdateTaskDto = {
+          title: 'Updated task',
+          isChecked: true,
+        };
+        await expect(controller.update(10, updatedTaskDTO)).resolves.toEqual({
+          id: 10,
+          ...updatedTaskDTO,
+        });
+      });
+
+      it('should return not found on missing task update', async () => {
+        jest
+          .spyOn(controller, 'update')
+          .mockRejectedValueOnce(new NotFoundException());
+
+        const updatedTaskDTO: UpdateTaskDto = {
+          title: 'Updated task',
+          isChecked: true,
+        };
+
+        await expect(controller.update(123, updatedTaskDTO)).rejects.toThrow(
+          NotFoundException,
+        );
+      });
+
+      it('should return error on invalid task update', async () => {
+        jest
+          .spyOn(controller, 'update')
+          .mockRejectedValueOnce(new BadRequestException());
+
+        const request = {
+          title: null,
+          isChecked: false,
+        };
+
+        await expect(controller.update(10, request)).rejects.toThrow(
+          BadRequestException,
+        );
+      });
+    });
+
+    describe('Delete', () => {
+      it('should remove a task', async () => {
+        await expect(controller.remove(20)).resolves.toEqual({
+          id: 20,
+          title: 'Deleted Task',
+          isChecked: false,
+        });
+      });
+
+      it('should return not found on missing task removal', async () => {
+        jest
+          .spyOn(controller, 'remove')
+          .mockRejectedValueOnce(new NotFoundException());
+
+        await expect(controller.remove(123)).rejects.toThrow(NotFoundException);
+      });
+    });
   });
 });
--- api/src/task/task.service.spec.ts
+++ api/src/task/task.service.spec.ts
@@ -1,15 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import { CreateTaskDto } from './dto/create-task.dto';
+import { Entity } from 'typeorm';
 import { Task } from './entities/task.entity';
 import { TaskService } from './task.service';
 
 describe('TaskService', () => {
   let service: TaskService;
+
+  const mockTask: Task = { id: 1, title: 'My mocked task', isChecked: false };
+
+  const tasksArray: Task[] = [
+    mockTask,
+    { id: 2, title: 'My other mocked task', isChecked: true },
+    { id: 3, title: 'My last mocked task', isChecked: true },
+  ];
+
   const mockRepository = {
     save: jest.fn(),
-    find: jest.fn(),
+    find: jest.fn().mockResolvedValue(tasksArray),
+    findOneBy: jest.fn().mockResolvedValue(mockTask),
+    update: jest.fn((entity) => entity),
+    remove: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -32,11 +43,11 @@ describe('TaskService', () => {
     });
 
     it('should have all methods defined', () => {
-      expect(service.findAll).toBeDefined();
       expect(service.create).toBeDefined();
+      expect(service.findAll).toBeDefined();
       expect(service.findOne).toBeDefined();
-      expect(service.remove).toBeDefined();
       expect(service.update).toBeDefined();
+      expect(service.remove).toBeDefined();
     });
   });
 
@@ -53,11 +64,41 @@ describe('TaskService', () => {
         expect(mockRepository.save).toBeCalledTimes(1);
       });
     });
+
     describe('Read', () => {
       it('should get all tasks', async () => {
-        await service.findAll();
+        const tasks = await service.findAll();
+        expect(tasks).toEqual(tasksArray);
+      });
 
-        expect(mockRepository.find).toBeCalledTimes(1);
+      it('should get single task', () => {
+        const repoSpy = jest.spyOn(mockRepository, 'findOneBy');
+        expect(service.findOne(1)).resolves.toEqual(mockTask);
+        expect(repoSpy).toBeCalledWith({ id: 1 });
+      });
+    });
+
+    describe('Update', () => {
+      it('should update a task', async () => {
+        const randomId = Math.floor(Math.random() * Math.floor(100));
+        const payload = {
+          title: 'Bye!',
+          isChecked: true,
+        };
+
+        await service.update(randomId, payload);
+
+        expect(mockRepository.update).toBeCalledTimes(1);
+      });
+    });
+
+    describe('Delete', () => {
+      it('should delete a task', async () => {
+        const randomId = Math.floor(Math.random() * Math.floor(100));
+
+        await service.remove(randomId);
+
+        expect(mockRepository.remove).toBeCalledTimes(1);
       });
     });
   });
