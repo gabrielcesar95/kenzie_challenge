9d52cf93a345f4f858357ef4d86c2901696f19bc
Gabriel Cesar Mello <95gabrielcesar@gmail.com>
2023-03-06 15:55:53 -0300

    fix: add placeholder for empty list of tasks

--- app/src/components/TaskList.tsx
+++ app/src/components/TaskList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { List } from 'rsuite';
+import { FlexboxGrid, List } from 'rsuite';
 import { Task } from '../pages/HomePage';
 import { TaskItem } from './TaskItem';
 
@@ -12,16 +12,26 @@ type TaskListProps = {
 export const TaskList: React.FC<TaskListProps> = ({ tasks, onCheck, onEdit }) => {
   return (
     <List>
-      {tasks.map((task) => (
-        <TaskItem
-          key={task.id}
-          id={task.id}
-          title={task.title}
-          isChecked={task.isChecked}
-          onCheck={() => onCheck(task)}
-          onEdit={() => onEdit(task)}
-        />
-      ))}
+      {
+        tasks.length > 0
+          ? tasks.map((task) => (
+            <TaskItem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              isChecked={task.isChecked}
+              onCheck={() => onCheck(task)}
+              onEdit={() => onEdit(task)}
+            />
+          ))
+          : (
+            <List.Item>
+              <FlexboxGrid align="middle">
+                  <p>Nenhuma tarefa encontrada</p>
+              </FlexboxGrid>
+            </List.Item>
+          )
+      }
     </List>
   );
 };
